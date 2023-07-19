4067fd150201912dd406f815dcd162bf031f6e61
Fermin <josefermin.hernandez@adevinta.com>
2023-07-19 19:15:07 +0200

    update tester.go

--- tester.go
+++ tester.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
 
 	url := "http://localhost:8080/ads"
-	method := "GET"
+	method := "POST"
+
+	payload := strings.NewReader(`{
+    "title": "t1",
+    "description": "d1",
+    "price": 1.1,
+    "author": {
+        "firstName": "Muka",
+        "secondName": "Lamejor Detodas",
+        "age": 10,
+        "email": "muka@gmail.com"
+    }
+}`)
 
 	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
+	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Basic bXVrYTpsYW1lam9yZGV0b2Rhcw==")
 
 	res, err := client.Do(req)
